48accf3cb0623474c6670a22ebeb7a524423fdfc
Hossein Ghaedi <35895522+hosseinm1@users.noreply.github.com>
2022-02-18 16:03:33 +0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -7,38 +7,37 @@ var cacheFiles = [
     'images/cat-2.jpeg',
     'images/cat-3.jpeg',
     'images/icon-512.png'
-
+    
 ];
 
-self.addEventListener('install',(e) =>{
-    console.log('[Service Worker] install');
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
     e.waitUntil(
         caches.open(cacheName).then((cache) => {
-            console.log('[service worker] cacheing all the files');
+            console.log('[Service Worker] Caching all the files');
             return cache.addAll(cacheFiles);
         })
     );
 });
 
-/* self.addEventListener('fetch', function (e) {
+// self.addEventListener('fetch', function (e) {
+//     e.respondWith(
+//         // check if the cache has the file
+//         caches.match(e.request).then(function (r) {
+//             console.log('[Service Worker] Fetching resource: ' + e.request.url);
+//             // 'r' is the matching file if it exists in the cache
+//             return r 
+//         })
+//     );
+// });
+
+self.addEventListener('fetch', function (e) {
     e.respondWith(
-        //check if the cache has the file
         caches.match(e.request).then(function (r) {
-            console.log('[service worker] fetching resource' + e.request.url);
-            // r is the matching file if it exists in the cache
-            return r
-        })
-    );
-}); */
-
-
-self.addEventListener('fetch', function (e){
-    e.respondWith(
-        caches.match(e.request).then(function (r){
-            //download the files if itsnot in the caches
-            return r || fetch(e.request).then(function (response){
-                // add new file to cache
-                return caches.open(cacheName).then(function (cache){
+            // Download the file if it is not in the cache, 
+            return r || fetch(e.request).then(function (response) {
+                // add the new file to cache
+                return caches.open(cacheName).then(function (cache) {
                     cache.put(e.request, response.clone());
                     return response;
                 });
